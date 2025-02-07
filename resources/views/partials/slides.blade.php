357daa60f7202659030bd77e2bc56c010090da44
Sumon Ahmed <bdsumon4u@gmail.com>
2025-02-07 23:50:35 +0600

    fix: correct style attribute quotes in slideshow component

--- resources/views/partials/slides.blade.php
+++ resources/views/partials/slides.blade.php
@@ -1,4 +1,4 @@
-<div class="block-slideshow block-slideshow--layout--with-departments block">
+<div class="block block-slideshow block-slideshow--layout--with-departments">
     <div id="slideshow-container" class="container">
         <div class="row">
             <div class="col-12 -col-lg-9 -offset-lg-3">
@@ -7,9 +7,9 @@
                         @foreach($slides as $slide)
                         <a class="block-slideshow__slide" href="{{ $slide->btn_href ?? '#' }}">
                             <div class="block-slideshow__slide-image block-slideshow__slide-image--desktop"
-                                style="background-image: url({{ asset($slide->desktop_src) }}); background-position: center;"></div>
+                                style="background-image: url('{{ asset($slide->desktop_src) }}'); background-position: center;"></div>
                             <div class="block-slideshow__slide-image block-slideshow__slide-image--mobile"
-                                style="background-image: url({{ asset($slide->mobile_src) }}); background-position: center;"></div>
+                                style="background-image: url('{{ asset($slide->mobile_src) }}'); background-position: center;"></div>
                             <div class="block-slideshow__slide-content">
                                 <div class="block-slideshow__slide-title">{!! $slide->title !!}</div>
                                 <div class="block-slideshow__slide-text">{!! $slide->text !!}</div>
